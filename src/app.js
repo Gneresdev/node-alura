93ea761b41ea5714f3539f58ae6beab4713138ad
gusta <gustavoneresemail@gmail.com>
2025-09-12 00:03:14 -0300

    83% em produção

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbconnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexao", erro);
+});
+
+conexao.on("open", () => {
+  console.log("conexao com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("Livro removido com sucesso.");
+});
+
+export default app;
